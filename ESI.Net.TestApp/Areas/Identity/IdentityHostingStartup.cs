b0177f48ee3be973ff53407a780fb5b5d37c1078
Anders Urban <minikaliffen@gmail.com>
2019-11-12 10:31:52 +0100

    Add project files.

---
+++ ESI.Net.TestApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ESI.Net.TestApp.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ESI.Net.TestApp.Areas.Identity.IdentityHostingStartup))]
+namespace ESI.Net.TestApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
